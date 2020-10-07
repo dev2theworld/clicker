91b23d048c58c005decc34619861ecc4b91a293a
Michał Wilk <dev2theworld@gmail.com>
2020-10-07 17:59:44 +0200

    Styling changes

--- main.js
+++ main.js
@@ -2,8 +2,7 @@ const toChange = document.querySelectorAll('.toChange');
 
 toChange.forEach((change) => {
   change.addEventListener('click', () => {
-    /*change.classList.add('bgclicked');
-    change.classList.remove('bg');*/
+    
     change.style.opacity = 0;
   });
 });
